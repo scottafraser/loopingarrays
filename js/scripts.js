52a657c0224f45ac2f2d825d6786db20a94bb910
David Ngo and Scott Fraser <scott@>
2018-06-04 13:14:15 -0700

    everything

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,74 @@
+
 $(function() {
+  $("#groceryList").show();
 
-  $("#favFood").submit(function(event) {
-    var favFood = [];
-    favFood.push($("#food1").val());
-    favFood.push($("#food2").val());
-    favFood.push($("#food3").val());
-    var listFood = [];
-    listFood.push(favFood.pop());
-    listFood.push(favFood.shift());
-    listFood.push(favFood.pop());
 
-    $('#list').append("<li>" + listFood[0] + "</li>");
-    $('#list').append("<li>" + listFood[1] + "</li>");
-    $('#list').append("<li>" + listFood[2] + "</li>");
+  // grocery js
+  $("#groceryList").submit(function(event) {
+    event.preventDefault();
+    var foods = ['food1', 'food2', 'food3'];
 
-    var iceCreams = ['vanilla', 'chocolate', 'strawberry'];
-    iceCreams.forEach(function(iceCream) {
-      $('#list').append("<li>" + iceCream + "</li>");
+    var listItems = [];
+
+    foods.forEach(function(food) {
+
+      listItems.push($("#" + food).val());
     });
 
+    listItems.sort();
+
+
+
+
+    var alphaList = listItems.map(function(listItem) {
+      return listItem.toUpperCase();
+
+    });
+
+
+    alphaList.forEach(function(x) {
+      $("#list").append("<li>" + x + "</li>");
+    })
+
+    $("#groceryList").hide();
+  });
+
+  // word js
+  $("#wordForm").submit(function(event) {
     event.preventDefault();
 
+    var userSentence = [];
+    userSentence = ($('#userSentence').val()).split(" ");
+
+    var longWords = [];
+
+    userSentence.forEach(function(word) {
+      if (word.length >= 3) {
+        longWords.push(word);
+      }
+    });
+
+    longWords.reverse();
+
+    var stupidSentance = longWords.join(" ");
+
+    $('#sillySentance').text(stupidSentance);
+
   });
+
+  //card
+
+  var suits = ['spades', 'clubs', 'hearts', 'diamonds'];
+  var values = ['ace', '2', '3', '4', '5', '6' , '7', '8', '9', '10', 'jack', 'queen', 'king'];
+  var deck = [];
+
+  suits.forEach(function(suit) {
+    values.forEach(function(value) {
+      deck.push(value + " of " + suit);
+    });
+  });
+
+  console.log(deck);
+
+
 });
